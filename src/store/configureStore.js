05f47a6b6e60a39ce421bcc315f0f312efa21772
galinagr <galinagr@yandex.ru>
2017-11-29 16:21:25 +0200

    update configureStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,10 @@
-import  { createStore, combineReducers, applyMiddleware } from 'redux';
+import  { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
 import authReducer from '../reducers/auth';
 
-const composeEnhansers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhansers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose;
 
 export default () => {
     const store = createStore(
